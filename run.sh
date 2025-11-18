3ee64213682518407e919f264c9629e36b390aed
Gianluca Guidi <gianluca@guidi.co.uk>
2025-11-18 22:04:40 +0000

    Parameterise directories in run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,52 @@
 #!/bin/bash
 
+set -e
+
+OUT_DIR=out
+RELEASE_CONFIRMATIONS=release-confirmations
+EXCHANGE_RATES=monthly-exchange-rates-by-hmrc
+
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        --out|-o)
+            OUT_DIR="$2"
+            shift 2
+            ;;
+        --release-confirmations|-r)
+            RELEASE_CONFIRMATIONS="$2"
+            shift 2
+            ;;
+        --exchange-rates|-x)
+            EXCHANGE_RATES="$2"
+            shift 2
+            ;;
+        -h|--help)
+            echo "Usage: $0 [--out DIR]"
+            echo
+            echo "Options:"
+            echo "  --out, -o DIR   Specify output directory (default: ./out)"
+            echo "  --release-confirmations, -r DIR   Specify directory that contains all the stock release confirmations (default: ./release-confirmations)"
+            echo "  --exchange-rates, -x DIR   Specify directory that contains the monthly exchange rates (default: ./monthly-exchange-rates-by-hmrc)"
+            echo "  -h, --help      Show this help message"
+            exit 0
+            ;;
+        *)
+            echo "Unknown parameter: $1"
+            echo "Use --help for usage information."
+            exit 1
+            ;;
+    esac
+done
+
+# Create directory if it doesn't exist
+mkdir -p "$OUT_DIR"
+
 source "${PWD}/env/bin/activate"
 echo $(which python3)
 
-python3 bin/parse-stock-releases.py release-confirmations/*/*.pdf > parsed-releases.csv
-python3 bin/combine.py monthly-exchange-rates-by-hmrc/*.csv > exchange-rates.csv
-python3 bin/calculate-cost-basis.py parsed-releases.csv exchange-rates.csv > cost-basis.csv
+
+python3 bin/parse-stock-releases.py $RELEASE_CONFIRMATIONS/*/*.pdf > $OUT_DIR/parsed-releases.csv
+python3 bin/combine.py $EXCHANGE_RATES/*.csv > $OUT_DIR/exchange-rates.csv
+python3 bin/calculate-cost-basis.py $OUT_DIR/parsed-releases.csv $OUT_DIR/exchange-rates.csv > $OUT_DIR/cost-basis.csv
 
